8ecd2dce0eecdb41c241dac3c92f932defa7fecc
grungebuddy <grungebuddy@users.noreply.github.com>
2023-09-21 00:51:22 +0700

    Update gpt-fluff-killer.py

--- gpt-fluff-killer.py
+++ gpt-fluff-killer.py
@@ -1,34 +1,24 @@
 import os
 import re
 
-# Specify the folder containing the .txt files to process
-# Note: Replace 'your_folder_path_here' with the actual path to your folder
-# Note: Use '\\' Example: C:\\Users\\Me\\textfiles
-folder_path = r'your_folder_path_here'
-process_files(folder_path)
-
 # Function to remove and replace specific phrases in the text
 def remove_and_replace_phrases(text, phrases_to_remove, phrases_to_replace):
-    # Replace specific phrases throughout the text
     for phrase, replacement in phrases_to_replace.items():
         regex_replace = re.compile(rf"\b{phrase}\b", re.IGNORECASE)
         text = regex_replace.sub(replacement, text)
         
-    # Remove phrases and handle the beginning of sentences
     for phrase in phrases_to_remove:
         regex_start = re.compile(rf"^{phrase}, ", re.IGNORECASE)
         text = regex_start.sub("", text)
         regex_else = re.compile(rf"\s{phrase}(\s|,|\.)", re.IGNORECASE)
         text = regex_else.sub(" ", text)
         
-    # Capitalize the first letter of each sentence
     text = re.sub(r"(^|\. )(.)", lambda m: m.group(1) + m.group(2).upper(), text)
     
     return text
 
 # Main function to process files in a given folder
 def process_files(folder_path):
-    # Phrases to remove from the text
     phrases_to_remove = [
         "furthermore",
         "additionally",
@@ -39,7 +29,6 @@ def process_files(folder_path):
         "first and foremost"
     ]
     
-    # Phrases to replace in the text
     phrases_to_replace = {
         "in addition to": "besides",
         "an informed decision": "a decision",
@@ -49,23 +38,19 @@ def process_files(folder_path):
         "learn about the details before making a decision": "learn more"
     }
     
-    # Create a new subfolder for processed files
     output_folder = os.path.join(folder_path, "Processed_TXT")
     os.makedirs(output_folder, exist_ok=True)
     
-    # Loop through each .txt file in the folder
     for filename in os.listdir(folder_path):
         if filename.endswith('.txt'):
             file_path = os.path.join(folder_path, filename)
-            
-            # Read the file content
             with open(file_path, 'r') as f:
                 content = f.read()
-            
-            # Remove and replace specified phrases
             modified_content = remove_and_replace_phrases(content, phrases_to_remove, phrases_to_replace)
-            
-            # Write the modified content to a new .txt file in the subfolder
             output_file_path = os.path.join(output_folder, filename)
             with open(output_file_path, 'w') as f:
                 f.write(modified_content)
+
+# Specify the folder containing the .txt files to process
+folder_path = r'C:\\Users\\Jay Pee\\Documents\\test'
+process_files(folder_path)
